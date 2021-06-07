c91e8a0d2432f876ff7f9d5bda1e07e659aa0820
OM Miles <miles@ommiles.com>
2021-06-07 16:09:01 -0700

    simplified version

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :students, only: :index
+  resources :students, only: [:index, :show]
 
   # Example of regular route:
   # The user is able to go to /students/2 to receive information on the student with an id of 2
-  get 'students/:id', to:'students#show'
+  # get 'students/:id', to:'students#show'
 
   # Example of named route that can be invoked with cohort_url(id: student.id)
   # get 'students/:id/cohort' => 'catalog#cohort', as: :cohort
